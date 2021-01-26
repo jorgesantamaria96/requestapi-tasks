874b52b367ed9a0d81a9b4c1d180bd47aea5b29b
jsantamaria <jsantamaria@zennovia.com>
2021-01-26 19:13:41 -0300

    Min: 55:31' uso de plugin de vscode para peticiones http, creacion y envio de tareas por peticiones

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import TasksRoutes from "./routes/tasks.routes";
 
 const app = express();
 
+// settings
 app.set("port", process.env.port || 3000);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("Wellcome to my application!");
 });
 
-app.use('/api/tasks', TasksRoutes);
+// routes
+app.use("/api/tasks", TasksRoutes);
 
 export default app;
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import Task from "../models/Tasks";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Tasks");
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+router.post("/", async (req, res) => {
+  const newTask = new Task({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  const taskSaved = await newTask.save();
+  res.json(taskSaved);
 });
 
 export default router;
